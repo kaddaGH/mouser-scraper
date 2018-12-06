60e6b806a59c9943871bbcae9e86828d84de4151
gkadda <ghouadni55@hotmail.fr>
2018-12-06 22:29:11 +0100

    Add parser script.

--- parsers/part_number_search.rb
+++ parsers/part_number_search.rb
@@ -1,41 +1,14 @@
-class ParserProductPage
-  attr_reader :content, :vars, :outputs, :pages, :page
+html = Nokogiri.HTML(content)
+product_details = {
+    _collection: 'products',
+    "ProductName" => html.css('span.bc-no-link')&.text,
+    "ProductDescription" => html.css('span#spnDescription')&.text&.strip,
+    "price" => content.match(/"Qty":1,"FormattedUnitPrice":"([^"]+?)"/) ? content.scan(/"Qty":1,"FormattedUnitPrice":"([^"]+?)"/)[0][0] : ""
+}
+# Extract extra details for the  product
+content.scan(/"Label":"([^"]+?):".+?"Value":"([^"]+?)"/).each do |specification|
 
-  def initialize(params = {})
-    @content = params[:content]
-    @vars = params[:vars]
-    @outputs = params[:outputs]
-    @pages = params[:pages]
-    @page = params[:page]
-  end
+  product_details[specification[0].sub(/[^A-Za-z]/, '')] = specification[1]
 
-  def parse
-    nokogiri = Nokogiri.HTML(content)
-
-    info_table = nokogiri.css('table#product-overview')
-
-    usd_prices_table = nokogiri.at_css('#priceProcurement .product-dollars')
-
-    product = {}
-
-    product['part_number'] = info_table.at_css('#reportPartNumber').text.strip
-    product['quantity'] = info_table.css('#quantityAvailable span').text.strip
-    product['description'] = info_table.at("//td[@itemprop = 'description']").text.strip
-    product['manufacturer_number'] = info_table.at("//h1[@itemprop = 'model']").text.strip
-    product['_collection'] = 'listings'
-
-    unless usd_prices_table.nil?
-      product['prices'] = usd_prices_table.css('tr')[1..-1].map do |price_info|
-        columns = price_info.css('td')
-
-        {
-            'price_break': columns.first.text.strip,
-            'unit_price': columns[1].text.strip,
-            'expected_price': columns.last.text.strip
-        }
-      end
-    end
-
-    outputs << product
-  end
 end
+outputs << product_details
